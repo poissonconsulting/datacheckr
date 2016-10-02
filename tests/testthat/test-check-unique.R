ec6af1250ea13917eb634423ee48783a95cc6bc9
Joe Thorley <joe@poissonconsulting.ca>
2016-10-02 12:09:38 -0700

    Add check_unique

---
+++ tests/testthat/test-check-unique.R
@@ -0,0 +1,8 @@
+context("check-unique")
+
+test_that("check_unique errors", {
+  expect_identical(check_unique(1), 1)
+  expect_identical(check_unique(1:2), 1:2)
+  expect_identical(check_unique(character(0)), character(0))
+  expect_error(check_unique(c(1,1)), "c[(]1, 1[)] must be unique")
+})
